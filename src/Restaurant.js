e9f2afc53035dd0d365deb242bef491d4e1e0a64
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-08-13 13:36:03 -0700

    Add button functionality showing only one button ata a time

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -4,15 +4,19 @@ import './Restaurant.css';
 
 class Restaurant extends Component {
   render () {
-    const { name, votes, handleSelect, handleDeselect  } = this.props;
+    const { name, votes, user, handleSelect, handleDeselect  } = this.props;
+    const userHasSelected = votes && Object.keys(votes).includes(user.uid)
     return (
       <article className="Restaurant">
-      <h3> { name } </h3>
-      <ul>
-        {votes && map(votes, (vote, key) => <li key={key}>{ vote }</li>)}
-      </ul>
-      <button onClick={handleSelect}>Vote for me</button>
-      <button className="destructive" onClick={handleDeselect}>Never mind</button>
+        <h3> { name } </h3>
+        <ul>
+          {votes && map(votes, (vote, key) => <li key={key}>{ vote }</li>)}
+        </ul>
+        {
+          userHasSelected
+          ? <button className="destructive" onClick={handleDeselect}>Never mind</button>
+          : <button onClick={handleSelect}>Vote for me</button>
+        }
       </article>
     );
   }
